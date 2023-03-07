d4af07b907148541b7c0381a831f1662b1feb20b
Osamah_Hasan <124389134+Osamah-Hasan@users.noreply.github.com>
2023-03-07 18:29:15 +0000

    update

---
+++ go_app/main.go
@@ -0,0 +1,15 @@
+// Created by: Osamah Hasan
+// Created on: Mar 2023
+//
+// This program displays, "Hello, World!"
+
+package main
+
+import "fmt"
+
+func main() {
+	  // This function displays, "Hello, World!"
+	fmt.Println("Hello, World!")
+
+	fmt.Println("\nDone.")
+}
